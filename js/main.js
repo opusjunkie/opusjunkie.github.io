62d477077314c4e11abbbafc9f3b2f014316631b
Mashood <mashoodwyd@gmail.com>
2023-02-22 09:07:07 +0400

    [IMP] added new images and some optimisation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 (function ($) {
     "use strict";
-    
-    console.log('kereeeeeeeeeeeeeeeeeeeee')
-    
-    // loader
-    var loader = function () {
+
+    // hide only profile information if the link contains profile
+    const url = window.location.href;
+    if (url.includes('portfolio')) {
+        $('.market').show();
+    } else {
+        $('.market').hide();
+    }
+
+    // Cache repeated selectors
+    const $backToTop = $('.back-to-top');
+    const $navbar = $('.navbar');
+    const $navbarLinks = $(".navbar-nav a");
+
+    // Loader
+    const loader = function () {
         setTimeout(function () {
             if ($('#loader').length > 0) {
                 $('#loader').removeClass('show');
@@ -12,73 +23,65 @@
         }, 1);
     };
     loader();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
+        const $this = $(this);
+        if ($this.scrollTop() > 200) {
+            $backToTop.fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $backToTop.fadeOut('slow');
+        }
+        if ($this.scrollTop() > 0) {
+            $navbar.addClass('nav-sticky');
+        } else {
+            $navbar.removeClass('nav-sticky');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+
+    $backToTop.click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-    
-    
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar').addClass('nav-sticky');
-        } else {
-            $('.navbar').removeClass('nav-sticky');
-        }
-    });
-    
-    
+
     // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
+    $navbarLinks.on('click', function (event) {
+        const hash = this.hash;
+        if (hash !== "") {
             event.preventDefault();
-            
+            const $this = $(this);
             $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
+                scrollTop: $(hash).offset().top - 45
             }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
+            if ($this.parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
+                $this.closest('a').addClass('active');
             }
         }
     });
-    
-    
+
     // Typed Initiate
-    if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
-    }
-    
-    
+//    const $typedH2 = $('.hero .hero-text h2');
+//    if ($typedH2.length === 1) {
+//        const typedStrings = $('.hero .hero-text .typed-text').text();
+//        const typed = new Typed($typedH2.get(0), {
+//            strings: typedStrings.split(', '),
+//            typeSpeed: 100,
+//            backSpeed: 20,
+//            smartBackspace: false,
+//            loop: true
+//        });
+//    }
+
     // Skills
     $('.skills').waypoint(function () {
         $('.progress .progress-bar').each(function () {
             $(this).css("width", $(this).attr("aria-valuenow") + '%');
         });
-    }, {offset: '80%'});
-
+    }, { offset: '80%' });
 
     // Testimonials carousel
     $(".testimonials-carousel").owlCarousel({
@@ -87,27 +90,22 @@
         dots: true,
         loop: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             }
         }
     });
-    
-    
-    
+
     // Portfolio filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
+    const $portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
     });
 
     $('#portfolio-filter li').on('click', function () {
+        const $this = $(this);
         $("#portfolio-filter li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        $this.addClass('filter-active');
+        $portfolioIsotope.isotope({ filter: $this.data('filter') });
     });
-
-
-    
 })(jQuery);
-
